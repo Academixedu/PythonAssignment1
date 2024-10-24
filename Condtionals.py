7b4cfa5eba07c016e68eee329d4ea310499ebed0
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-10-24 14:54:56 +0530

    Create Condtionals.py

---
+++ Condtionals.py
@@ -0,0 +1,11 @@
+# Problem: Write a Python program that asks the user for a number and checks whether it is odd or even. Print "Odd" or "Even" accordingly.
+# Write a Python program that takes a number as input and prints whether the number is positive, negative, or zero.
+# Write a program that takes a student's marks as input and prints their grade based on the following scale:
+# 90 and above: A
+# 80 to 89: B
+# 70 to 79: C
+# 60 to 69: D
+# Below 60: F
+ # Write a Python program that takes three numbers as input and prints the largest of the three.
+# Write a Python program that takes a letter as input and checks whether it is a vowel or a consonant.
+#  Write a Python program that acts as a simple calculator. It should take two numbers and an operator (+, -, *, /) as input, and then perform the appropriate operation.

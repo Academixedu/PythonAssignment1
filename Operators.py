0d1483a52affd4dcac967da7209889657990130c
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-10-24 14:51:20 +0530

    Create Operators.py

---
+++ Operators.py
@@ -0,0 +1,8 @@
+# Write a Python program that takes two numbers from the user and performs addition, subtraction, multiplication, and division. Display the results for each operation.
+#  Write a Python program to calculate the remainder and the quotient of two numbers. Quotient Should be an Integer
+# Given a number, write a program that calculates its square and cube.
+# Start with a variable x = 10. Use assignment operators to increment x by 5, multiply it by 3, and subtract 4. Print the final value of x.
+# Write a Python program that takes a number from the user and doubles it using the assignment operator.
+# Write a Python program to find the bitwise AND, OR, and XOR of two numbers.
+# Write a program to left shift a number by 2 positions and right shift it by 3 positions. Display both results.
+# Write a Python program that takes a number, adds 7, multiplies it by 3, divides by 2, and prints the result.

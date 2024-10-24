261f6ab144be5685e025ecae46f733c5eaa99786
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-10-24 14:46:07 +0530

    Create Basics.py

---
+++ Basics.py
@@ -0,0 +1,6 @@
+# Please Answer the Below Questions and Give Your Explanation in One Line After Completing the Assignment
+ # Write a Python program that assigns your name and age to two different variables. Then, reassign the age variable with a new value.
+ # Assign the value of x = 10. Reassign it to x + 5 and print the result.
+# Take a number from the user as input. The input is a string by default. Convert it into an integer and print the result.
+# Assign a float value to a variable, then cast it to an integer. Print both the float and the integer values.
+# Write a Python program that prompts the user to enter their age, and then cast the input to an integer. After that, assign their age in the next decade (age + 10) to a new variable and print it.

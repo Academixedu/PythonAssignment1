104764af81afaa59a0023176f77b176ccf42d4ca
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-10-24 15:01:50 +0530

    Create Loopings.py

---
+++ Loopings.py
@@ -0,0 +1,4 @@
+# Write a Python program to find the sum of the first n natural numbers, where n is provided by the user.
+# Write a Python program to print the multiplication table of a number entered by the user, up to 10.
+# Write a Python program that prints all even numbers between 1 and n, where n is provided by the user.
+# Write a Python program to print numbers from n to 1, where n is provided by the user.

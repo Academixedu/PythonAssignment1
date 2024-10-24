fe0a1a60841c78eb27d7031d67e1a33d1011fd01
Ram <ramgovind.mallu@gmail.com>
2024-10-24 20:06:17 +0530

    Done required changes

--- Condtionals.py
+++ Condtionals.py
@@ -9,3 +9,72 @@
  # Write a Python program that takes three numbers as input and prints the largest of the three.
 # Write a Python program that takes a letter as input and checks whether it is a vowel or a consonant.
 #  Write a Python program that acts as a simple calculator. It should take two numbers and an operator (+, -, *, /) as input, and then perform the appropriate operation.
+
+# number = input("enter the number")
+# result = int(number)
+# result1=result%2
+# if result1 == 0:
+#     print ("Given number is even number")
+# else :
+#     print("Given number is odd number")
+
+# number = input ("enter the number: ")
+# result =int(number)
+# if result>0:
+#     print("given number is positive")
+# elif result<0 :
+#     print ("given number is negative")
+# else :
+#     print ("given number is zero")
+
+# marks=input("enter the student marks :")
+# result =int(marks)
+# if result>=90 :
+#     print("Student grade is A")
+# elif result <89 and result>=80 :
+#     print("Student grade is B")
+# elif result <79 and result>=70 :
+#     print("Student grade is C")
+# elif result <69 and result>=60 :
+#     print("Student grade is D")
+# elif result<60 :
+#     print("Student grade is F")
+
+# number1 = input("enter the number: ")
+# number2 = input("enter the number: ")
+# number3 = input("enter the number: ")
+# if number3>number2 and number1 :
+#     print( number3, "is greater number")
+# elif number2>number1 :
+#     print( number2, "is greater number")
+# else :
+#     print( number1, "is greater number")
+
+# letter = input("Enter the letter: ")
+# print(letter)
+# if letter == "a" :
+#     print("the given letter is vowel")
+# elif letter == "e" :
+#     print("the given letter is vowel")
+# elif letter == "i" :
+#     print("the given letter is vowel")
+# elif letter == "o" :
+#     print("the given letter is vowel")
+# elif letter == "u" :
+#     print("the given letter is vowel")
+# else :
+#     print ("the given letter is consonant")
+
+number1 = int(input("enter the number: "))
+number2 = int (input("enter the number: "))
+operator = input("enter the operator: ")
+if operator == "+" :
+    print(number1 + number2)
+elif operator == "-" :
+    print(number1 - number2)
+elif operator == "*" :
+    print(number1 * number2)
+elif operator == "/" :
+    print(number1 / number2)
+else :
+    print("User given operator is not supported")
--- Operators.py
+++ Operators.py
@@ -2,7 +2,37 @@
 #  Write a Python program to calculate the remainder and the quotient of two numbers. Quotient Should be an Integer
 # Given a number, write a program that calculates its square and cube.
 # Start with a variable x = 10. Use assignment operators to increment x by 5, multiply it by 3, and subtract 4. Print the final value of x.
+# 
 # Write a Python program that takes a number from the user and doubles it using the assignment operator.
-# Write a Python program to find the bitwise AND, OR, and XOR of two numbers.
+# Write a Python program to find the bitwise AND, OR, and XOR of two numbers.()
 # Write a program to left shift a number by 2 positions and right shift it by 3 positions. Display both results.
+# 
 # Write a Python program that takes a number, adds 7, multiplies it by 3, divides by 2, and prints the result.
+
+# a= int(input("enter the number: "))
+# b= int (input("enter the number: "))
+# print ("addition is ", a+b) 
+# print ("subtraction is ", a-b)
+# print ("multiplication is", a*b)
+# print ("division is ", a/b)
+
+# a= int(input("enter the number: "))
+# b= int (input("enter the number: "))
+# print ("remainder is " ,a%b)
+# print ("quotient is" , int(a/b))
+
+# a= int(input("enter the number: "))
+# print ("Squere is " ,a**2)
+# print ("Cube is" , a**3)
+
+# x = 10
+# a=x+5
+# b=a*3
+# c=b-4
+# print("final result of x is ", c)
+
+a= int(input("enter the number: "))
+b=a+7
+c=b*3
+d=c/2
+print("final result of a number givin by user is ", d)

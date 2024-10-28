db444a8b461fd3b1849daca369341c6f57d99849
babitha0305 <marpally.babithagoud@gmail.com>
2024-10-29 00:37:17 +0530

    completed the assignment

--- Basics.py
+++ Basics.py
@@ -4,3 +4,36 @@
 # Take a number from the user as input. The input is a string by default. Convert it into an integer and print the result.
 # Assign a float value to a variable, then cast it to an integer. Print both the float and the integer values.
 # Write a Python program that prompts the user to enter their age, and then cast the input to an integer. After that, assign their age in the next decade (age + 10) to a new variable and print it.
+
+#1
+a = "Babitha"
+b = 22
+print(a)
+print(b)
+b  = float(b)
+print(b)
+
+#2
+x=10
+y= x+5
+print(y)
+
+#3
+z=20
+z=str(z)
+print(type(z))
+#cannot convert string to an int
+
+#4
+n=25.8
+print(n)
+n=int(n)
+print(n)
+
+#5
+p= 18
+print("Enter your age: ")
+p = int(p)
+print(p)
+m= p + 10
+print(m)

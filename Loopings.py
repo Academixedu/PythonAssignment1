62b8a3a449304cec630eb8a088635647c9331511
babitha0305 <marpally.babithagoud@gmail.com>
2024-10-29 07:57:50 +0530

    assignment completed

--- Loopings.py
+++ Loopings.py
@@ -2,3 +2,29 @@
 # Write a Python program to print the multiplication table of a number entered by the user, up to 10.
 # Write a Python program that prints all even numbers between 1 and n, where n is provided by the user.
 # Write a Python program to print numbers from n to 1, where n is provided by the user.
+
+#1
+
+n = 4
+sum = n
+x = 0 
+for i in range(1, n + 1):  
+    x += i  
+print(x)
+
+#2
+# y =int(input("Enter a number: "))
+
+# for n in range(1, 11):
+#     print(y, "x", n, "=", y * i)
+    
+#3
+# w = int(input("Enter a number: "))
+
+# for i in range(2, w + 1, 2):  
+#     print(i)
+    
+#4
+n = int(input("Enter a number: "))
+for i in range(n, 0, -1):  
+    print(i)
--- Operators.py
+++ Operators.py
@@ -6,3 +6,84 @@
 # Write a Python program to find the bitwise AND, OR, and XOR of two numbers.
 # Write a program to left shift a number by 2 positions and right shift it by 3 positions. Display both results.
 # Write a Python program that takes a number, adds 7, multiplies it by 3, divides by 2, and prints the result.
+
+#1
+# a = int(input("Enter the 1st value: "))
+# b= int(input("Enter the 2nd value: "))
+# d =a+b
+# print(d)
+# d= a-b
+# print(d)
+# d=a*b
+# print(d)
+# d=a/b
+# print(d)
+
+# #2
+# x=5
+# y=2
+# d=x%y
+# print(d)
+# d = x//y
+# print(d)
+
+#3
+# number = int(input("Enter a number: "))
+
+
+# square = number ** 2
+# print("Square of", number, "is:", square)
+# cube = number ** 3
+# print("Cube of", number, "is:", cube)
+
+# #4
+# x = 10
+
+# x += 5
+
+# x *= 3
+
+# x -= 4
+
+# print(x)
+
+#5
+# x = int(input("Enter a number: "))
+
+# y=x*2
+# print(y)
+
+#6
+# m = int(input("Enter the first number: "))  
+# n = int(input("Enter the second number: "))  
+
+# y= m & n
+# print(y)
+# y = m | n
+# print(y)
+# y = m ^ n
+# print(y)
+
+#7
+# x = 6
+
+# left_shift = x << 2
+# print(left_shift)
+
+# right_shift =x >> 3
+# print(right_shift)
+
+#8
+
+number = float(input("Enter a number: " ))
+
+number += 7          
+number *= 3          
+number /= 2          
+
+print(number)
+
+
+
+
+
